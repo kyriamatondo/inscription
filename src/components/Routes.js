c524b7b93c3d95cb2cc97176b332b98114d7efa0
kyriamatondo <krmatondo@gmail.com>
2021-10-25 16:22:11 -0700

    to  remove

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 import Home from "./Home.js";
-import Users from "./Users.js";
-import Menu from "./Menu.js";
 import Navbar from '../Navbar/Navbar.js';
 import Eleve from '../Eleve/Eleve.js';
 
